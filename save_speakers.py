252343161ab9def378526fdee5e280f95b1fbfe0
izuchi <ryousuke201102@gmail.com>
2024-12-14 22:55:32 +0900

    mainの処理を追加

--- save_speakers.py
+++ save_speakers.py
@@ -3,6 +3,7 @@ import requests
 
 API_SERVER = "http://127.0.0.1:50032"
 
+
 def save_speakers():
     """
     利用可能なSpeaker情報をファイル出力する
@@ -10,15 +11,15 @@ def save_speakers():
     speakers = []
 
     response = requests.get(
-        f"{API_SERVER}/v1/speaker"
+        f"{API_SERVER}/v1/speakers",
     )
 
     for item in json.loads(response.content):
         speaker = {
-            "speakerName": item["speakName"],
-            "speakerUuid": item["speakUuid"],
+            "speakerName": item["speakerName"],
+            "speakerUuid": item["speakerUuid"],
             "styles": [
-                {"styleName": style["styleName"], "styleId": style["styleIId"]}
+                {"styleName": style["styleName"], "styleId": style["styleId"]}
                 for style in item["styles"]
             ],
             "version": item["version"],
@@ -26,6 +27,9 @@ def save_speakers():
 
         speakers.append(speaker)
 
-    # 出力ファイルの保存
+    # 出力ファイルに保存
     with open("speakers.json", "w", encoding="utf-8") as f:
         json.dump(speakers, f, ensure_ascii=False, indent=4)
+
+if __name__ == "__main__":
+    save_speakers()
